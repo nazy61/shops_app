bbd23111d3fced203ab45f933975b3fa3ff77433
MrTech <ogwo.chinaza@yahoo.com>
2019-11-24 20:30:25 +0100

    added orders screen and cart screen

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -3,7 +3,9 @@ import 'package:provider/provider.dart';
 
 import '../widgets/badge.dart';
 import '../widgets/products_grid.dart';
+import '../widgets/app_drawer.dart';
 import '../providers/cart.dart';
+import '../screens/cart_screen.dart';
 
 enum FilterOptions {
   Favorites,
@@ -31,7 +33,9 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
             ),
             child: IconButton(
               icon: Icon(Icons.shopping_cart),
-              onPressed: () {},
+              onPressed: () {
+                Navigator.of(context).pushNamed(CartScreen.routeName);
+              },
             ),
           ),
           PopupMenuButton(
@@ -56,6 +60,7 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
           ),
         ],
       ),
+      drawer: AppDrawer(),
       body: ProductsGrid(_showOnlyFavorites),
     );
   }
