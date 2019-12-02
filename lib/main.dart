52a2e02ee98bb4f7fc374429454a10cb8b757501
MrTech <ogwo.chinaza@yahoo.com>
2019-12-02 09:13:42 +0100

    implemented custom page route animation

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ import './screens/user_products_screen.dart';
 import './screens/edit_product_screen.dart';
 import './screens/auth_screen.dart';
 import './screens/splash_screen.dart';
+import './helpers/custom_route.dart';
 
 void main() => runApp(MyApp());
 
@@ -48,7 +49,11 @@ class MyApp extends StatelessWidget {
           theme: ThemeData(
               primarySwatch: Colors.purple,
               accentColor: Colors.deepOrange,
-              fontFamily: 'Lato'),
+              fontFamily: 'Lato',
+              pageTransitionsTheme: PageTransitionsTheme(builders: {
+                TargetPlatform.android: CustomPageTransitionBuilder(),
+                TargetPlatform.iOS: CustomPageTransitionBuilder(),
+              },),),
           home: auth.isAuth
               ? ProductsOverviewScreen()
               : FutureBuilder(
